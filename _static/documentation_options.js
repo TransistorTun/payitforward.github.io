868376eaf0a7d34d8c449d72760b2cefcf1f951f
TransistorTun <TransistorTun@users.noreply.github.com>
2025-03-08 07:05:59 +0000

    deploy: 25b180825479d336bb98d4c617c7520d63dba276

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.01',
+    VERSION: '1.0.1',
     LANGUAGE: 'Eng',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
